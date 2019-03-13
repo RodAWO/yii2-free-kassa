d3d878c4500095b25260a84802c152211de42efc
RodAWO <admin@onlineaw.ru>
2019-03-13 12:56:56 +0300

    ip user

--- src/Merchant.php
+++ src/Merchant.php
@@ -286,7 +286,7 @@ class Merchant extends Component
      */
     public function processResult($data)
     {
-        if (null !== $this->systemIPs && !$this->checkIp(\Yii::$app->request->getUserIP())) {
+        if (null !== $this->systemIPs && !$this->checkIp(\Yii::$app->request->userIP)) {
             throw new ForbiddenHttpException("Not allowed IP address");
         }
 
